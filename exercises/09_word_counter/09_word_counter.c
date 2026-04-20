4ac70254e75e6de127b03fe8c2cdc93eb6cc0129
Hokfs <Hokfs666@gmail.com>
2026-04-20 19:45:24 +0800

    Update 09_word_counter.c

--- exercises/09_word_counter/09_word_counter.c
+++ exercises/09_word_counter/09_word_counter.c
@@ -4,7 +4,18 @@ int main() {
     char str[]="Don't ask what your country can do for you, but ask what you can do for your country.";
     int wordCount = 0;
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+
+    int found = 0;
+    int i=0;
+    while (str[i]!='\0') {
+        if (str[i]==' ') {
+            found = 0;
+        }else if (found==0) {
+            wordCount++;
+            found = 1;
+        }
+        i++;
+    }
     
     printf("单词数量: %d\n", wordCount);
     
