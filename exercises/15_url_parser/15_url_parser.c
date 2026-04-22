ddc23be782555e57a2ef62f1af5593da7336fcce
Hokfs <Hokfs666@gmail.com>
2026-04-22 16:44:58 +0800

    Update 15_url_parser.c

--- exercises/15_url_parser/15_url_parser.c
+++ exercises/15_url_parser/15_url_parser.c
@@ -13,8 +13,38 @@ int parse_url(const char* url) {
     int err = 0;
 
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    const char*query_start = strchr(url, '?');
+    if (query_start == NULL) {
+        goto exit;
+    }
+    query_start = query_start + 1;
 
+    char *query_copy = (char *)malloc(strlen(query_start) + 1);
+    if (query_copy == NULL) {
+        err = -1;
+        goto exit;
+    }
+    strcpy(query_copy, query_start);
+
+    char *pair= strtok(query_copy, "&");
+
+    while (pair != NULL) {
+        char *equal = strchr(pair, '=');
+        if (equal != NULL) {
+            *equal = '\0';
+
+            char *key = pair;
+            char *value = equal + 1;
+            printf("%s=%s\n", key,value);
+        }else {
+            printf("%s\n", pair);
+        }
+        pair = strtok(NULL, "&");
+    }
+    free(query_copy);
+
+
+    
 exit:
     return err;
 }
